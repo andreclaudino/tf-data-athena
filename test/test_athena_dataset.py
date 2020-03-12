1893a97d899049a0b4a8b03e6f3cbaef5fdfaf5d
André Claudino <andre.claudino@b2wdigital.com>
2020-03-12 14:46:59 -0300

    correct default values for csv

--- test/test_athena_dataset.py
+++ test/test_athena_dataset.py
@@ -1,10 +1,10 @@
 import unittest
 
+import numpy as np
 import tensorflow as tf
 
 from test.mocks.mock_athena_connector import MockAthenaConnector
-from tf_data_athena.aws_persistense.athena_dataset import _create_athena_dataset_from_connector
-import numpy as np
+from tf_data_athena.aws_persistense.util_functions import create_athena_dataset_from_connector
 
 
 class TestAthenaDataset(unittest.TestCase):
@@ -26,7 +26,7 @@ class TestAthenaDataset(unittest.TestCase):
 
         connector = MockAthenaConnector().set_output_file(output_path).set_columns_data(columns_data)
 
-        dataset = _create_athena_dataset_from_connector(query, connector, num_parallel_calls=1)
+        dataset = create_athena_dataset_from_connector(query, connector, num_parallel_calls=1)
         actuals = list(dataset.batch(3).take(2).as_numpy_iterator())[:2]
 
         actual0 = actuals[0]
@@ -62,7 +62,7 @@ class TestAthenaDataset(unittest.TestCase):
 
         connector = MockAthenaConnector().set_output_file(output_path).set_columns_data(columns_data)
 
-        dataset = _create_athena_dataset_from_connector(query, connector, num_parallel_calls=1)
+        dataset = create_athena_dataset_from_connector(query, connector, num_parallel_calls=1)
 
         actuals = list(dataset.batch(3).take(2).as_numpy_iterator())[:2]
 
@@ -70,14 +70,15 @@ class TestAthenaDataset(unittest.TestCase):
         actual1 = actuals[1]
 
         expected0 = (np.array([b'false', b'false', b''], dtype=object),
-                     np.array([2, None, 21], dtype=np.int32),
+                     np.array([ 2., np.nan, 21.], dtype=np.float32),
                      np.array([b'validate', b'opened', b'opeded'], dtype=object),
                      np.array([b'', b'2020-01-10', b'2020-01-10'], dtype=object))
 
         expected1 = (np.array([b'true', b'false', b'false'], dtype=object),
-                     np.array([1, None, 106], dtype=np.int32),
+                     np.array([1.0, np.nan, 106.0], dtype=np.float32),
                      np.array([b'validate', b'opened', b''], dtype=object),
                      np.array([b'2020-01-10', b'2020-01-09', b'2020-01-10'], dtype=object))
 
-        assert np.array_equal(actual0, expected0)
-        assert np.array_equal(actual1, expected1)
+        np.testing.assert_equal(actual0, expected0, verbose=True)
+        np.testing.assert_equal(actual1, expected1, verbose=True)
+
--- tf_data_athena/aws_persistense/util_functions.py
+++ tf_data_athena/aws_persistense/util_functions.py
@@ -1,14 +1,16 @@
+import numpy as np
 import tensorflow as tf
+
 from tf_data_athena.aws_persistense.athena_dataset import AthenaDataset
 
 
 def create_athena_dataset_from_connector(query, athena_connector,
-                                         num_parallel_calls=tf.data.experimental.AUTOTUNE, **kwargs):
+                                         num_parallel_calls=1, **kwargs):
     raw_dataset = AthenaDataset(query, athena_connector, **kwargs)
     record_defaults = _make_record_defaults(raw_dataset.column_types)
 
     # Skip header and parse CSV lines
-    return raw_dataset.skip(1).map(lambda u: tf.io.decode_csv(u, record_defaults), num_parallel_calls)
+    return raw_dataset.skip(1).map(lambda u: tf.io.decode_csv(u, record_defaults, na_value=""), num_parallel_calls)
 
 
 @tf.function
@@ -18,4 +20,7 @@ def _make_record_defaults(column_types):
 
 @tf.function
 def _default_for_type(dtype):
-    return tf.constant((), shape=(0,), dtype=dtype)
+    if dtype == tf.string:
+        return tf.constant(value=[""], shape=(1,), dtype=dtype)
+
+    return tf.constant(value=[np.nan], shape=(1,), dtype=tf.float32)
